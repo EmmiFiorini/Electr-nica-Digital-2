a04b921fa56b38fa3dee8f9da299dab15414fe3f
juanakallis <juanakallis13@gmail.com>
2025-09-05 11:46:47 -0300

    ej 3 timer

--- Guia 1/Ejercicio3 (timer)/Ejercicio3_timers.c
+++ Guia 1/Ejercicio3 (timer)/Ejercicio3_timers.c
@@ -1,13 +1,19 @@
 #include <Ejercicio3_timers.h>
 
-void main()
-{
-
-	while(TRUE)
-	{
+#fuses INTRC_IO   // Utilizo oscilador interno con pines RA6 y RA7 como GPIO
+#fuses NOMCLR     // Desactivo el MCLR
+#fuses NOWDT      // Desactivo el watchdog
 
 
-		//TODO: User Code
-	}
+/*****************************************************************************
+ * Funciones de Inicializacion de Perifericos
+ ****************************************************************************/
 
-}
+void InitGPIO(void);
+
+void InitTimer0(void);
+
+/*****************************************************************************
+ * Interrupcion
+ ****************************************************************************/
+
