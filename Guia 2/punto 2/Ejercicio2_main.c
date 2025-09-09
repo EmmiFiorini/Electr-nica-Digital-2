fb08dea37d4d48121d6bfe390c0640adb6538ea4
EmmiFiorini <143649576+EmmiFiorini@users.noreply.github.com>
2025-09-09 14:55:09 -0300

    punto 1 y2

---
+++ Guia 2/punto 2/Ejercicio2_main.c
@@ -0,0 +1,101 @@
+#include <Ejercicio2_main.h>
+
+
+#fuses INTRC_IO   // Oscilador interno con pines RA6 y RA7 como GPIO
+#fuses NOMCLR     // Desactivo el MCLR
+#fuses NOWDT      // Desactivo el watchdog
+
+/***************************
+ * LCD
+ **************************/
+
+#define LCD_ENABLE_PIN  PIN_B0
+#define LCD_RS_PIN      PIN_B1
+#define LCD_RW_PIN      PIN_B2 // CONECTADO A GND SI NO LO USAMOS
+#define LCD_DATA4       PIN_B4
+#define LCD_DATA5       PIN_B5
+#define LCD_DATA6       PIN_B6
+#define LCD_DATA7       PIN_B7
+#include <lcd.c>
+
+
+/***************************
+ * Funciones de Inicializacion de Perifericos
+ **************************/
+
+void Init_GPIO();
+
+/***************************
+•?  ?Estados
+**************************/
+typedef enum {
+    Espero,
+    Prendido,
+    Apagado,
+} eEstado;
+
+eEstado estado_actual = Espero ;
+
+/***************************
+•?  ?Funciones
+**************************/
+
+void maquina(void);
+
+void main()
+{
+
+Init_GPIO();
+lcd_init();
+   while(TRUE) {
+   maquina();
+   }
+
+}
+
+void Init_GPIO()
+{
+/* SETEAMOS LOS PINES PB0-PB7 COMO SALIDA */
+   set_tris_b(0x00);
+   
+/* SETEAMOS LOS PINES RA0 y RA1 como entrada */
+   set_tris_a(0b00000011);
+   output_low(PIN_A0);
+   output_low(PIN_A1);
+}
+
+void maquina() {
+ 
+ switch(estado_actual) {
+ 
+  case Espero:
+   if(input(PIN_A0) == 1) {
+   printf(LCD_PUTC,"\f");
+   estado_actual = Prendido;
+   }
+  break;
+ 
+  case Prendido:
+ 
+  printf(LCD_PUTC,"Prendiendo \f");
+  
+  if(input(PIN_A1) == 1) {
+   estado_actual = Apagado;
+   }
+  break;
+ 
+  case Apagado:
+  
+  printf(LCD_PUTC,"Apangando \f");
+  
+  if(input(PIN_A0) == 1) {
+   printf(LCD_PUTC,"\f");
+   estado_actual = Prendido;
+   }
+   break;
+ 
+  default:
+  estado_actual = Espero;
+ }
+ 
+}
